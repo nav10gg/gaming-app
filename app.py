31ec55bc8e4aeffc00cc434a458c62d6eb2a687a
nav10gg <vanness.joshua22@gmail.com>
2026-04-21 23:12:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ st.info("*Oh, look who decided to check their stats. Still 4th place? Groundbrea
 
 # --- THE GAME MODE DICTIONARY ---
 GAME_MODES = {
-    "Ranked Resurgence": {"raw_tab": "Resurgence", "is_quads": True},
-    "Ranked WZ Quads": {"raw_tab": "WZ Quads", "is_quads": True},
-    "Ranked Black Ops Royale": {"raw_tab": "Black Ops Royale", "is_quads": True},
-    #"Ranked Duo Low": {"raw_tab": "Duo Low", "is_quads": False},
-    #"Ranked Duo High": {"raw_tab": "Duo High", "is_quads": False}
+    "Resurgence": {"raw_tab": "Resurgence", "is_quads": True},
+    "WZ Quads": {"raw_tab": "WZ Quads", "is_quads": True},
+    "Black Ops Royale": {"raw_tab": "Black Ops Royale", "is_quads": True},
+    #"Duo Low": {"raw_tab": "Duo Low", "is_quads": False},
+    #"Duo High": {"raw_tab": "Duo High", "is_quads": False}
 }
 
 # 3. Secure Connection to Google
